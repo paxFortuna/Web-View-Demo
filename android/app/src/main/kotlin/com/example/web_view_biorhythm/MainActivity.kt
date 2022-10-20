772146875b943946fb1efb677a7e2fba6d3d50c4
fortuna <avidia38@gmail.com>
2022-10-20 11:51:23 +0900

    first: javaScriptMode, native android 수정

---
+++ android/app/src/main/kotlin/com/example/web_view_biorhythm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_view_biorhythm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue,
+      ),
+      home: const App(),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const WebView(
+      // initialUrl: 'https://flutter.dev',
+      initialUrl: 'https://naver.com',
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
