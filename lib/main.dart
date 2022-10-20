ef4fa2fd772139eccbf74939c98bf37906d5447e
fortuna <avidia38@gmail.com>
2022-10-20 12:02:25 +0900

    feat: webview_contrller

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
       home: const App(),
     );
@@ -30,6 +31,8 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  late WebViewController _controller;
+
   @override
   void initState() {
     super.initState();
@@ -39,10 +42,46 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return const WebView(
-      // initialUrl: 'https://flutter.dev',
-      initialUrl: 'https://naver.com',
-      javascriptMode: JavascriptMode.unrestricted,
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _controller.goBack();
+                },
+                child: const Text('<-'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _controller.goForward();
+                },
+                child: const Text('->'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _controller.loadUrl('https://google.com');
+                },
+                child: const Text('google'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: WebView(
+              // initialUrl: 'https://flutter.dev',
+              initialUrl: 'https://naver.com',
+              // naver등 제한 설정된 url 해제하기
+              javascriptMode: JavascriptMode.unrestricted,
+              // 앱에서 웹뷰 콘트롤러 설정
+              onWebViewCreated: (WebViewController controller) {
+                _controller = controller;
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
